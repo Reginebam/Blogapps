e0000f197fad72b7cefdd3cc69b4ee6fbe89bd75
unknown <reginebam@gmail.com>
2020-10-26 12:33:32 +0000

    firstblog

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import {
+BrowserRouter, Switch, Route} from "react-router-dom";
+
+const Routes=()=>{
+
+
+}
+
+function App() {
+  return (
+    <div >
+      
+   blog
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "propstypes";
+
+function Home() {
+
+return(
+<div>
+    blog2
+</div>
+
+)
+
+
+}
+
+
+
+
+export default Home;
